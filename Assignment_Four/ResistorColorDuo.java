9636d94f7038093b03d52f97996d75ee37e38428
Dhanushraj <131767860+87Dhanush@users.noreply.github.com>
2024-11-05 19:42:00 +0530

    Update ResistorColorDuo.java

--- Assignment_Four/ResistorColorDuo.java
+++ Assignment_Four/ResistorColorDuo.java
@@ -3,7 +3,7 @@ public class ResistorColorDuo {
         String[] colors={"black","brown","red","orange","yellow","green","blue","violet","grey","white"};  
         System.out.print("Enter the 2 digit number : ");
         int digit=new java.util.Scanner(System.in).nextInt();
-        System.out.println("The Encoded color is "+colors[digit/10]+""+colors[digit%10]);
+        System.out.println("The Encoded color is "+colors[digit/10]+" and "+colors[digit%10]);
 
     }
 }
