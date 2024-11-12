4f6c9d417296496010dfc4552a174664e50bb54b
Dhanushraj <131767860+87Dhanush@users.noreply.github.com>
2024-11-12 17:49:58 +0530

    Add files via upload

---
+++ Assignment_Four/BottleSong.java
@@ -0,0 +1,11 @@
+public class BottleSong{
+    public static void main(String[] args) {
+         String[] array={"Ten","Nine","Eight","Seven","Six","Five","Four","Three","Two","One"};
+         for(String i:array){
+            System.out.println(i+" "+"green bottles hanging on the wall");
+            System.out.println(i+" "+"green bottles hanging on the wall");
+            System.out.println("And if one green bottle should accidentally fall,\n" +
+                                "There'll be nine green bottles hanging on the wall.");
+         }       
+    }
+}
